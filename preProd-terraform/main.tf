8f3c6ae039e59f52c2b71241ac187ab42ba70ac4
L-Christ-ASD <christ.devops75@gmail.com>
2025-03-21 00:28:45 +0100

    typo4

--- preProd-terraform/main.tf
+++ preProd-terraform/main.tf
@@ -135,6 +135,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in_myip" {
   to_port           = 22
 }
 
+# temporairement pour le workflow
+#resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in_myip" {
+#  security_group_id = aws_security_group.admin_ssh_preprod.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  from_port         = 22
+#  ip_protocol       = "tcp"
+#  to_port           = 22
+#}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in" {
   for_each          = toset(var.admin-ips)
   security_group_id = aws_security_group.admin_ssh_preprod.id
@@ -150,3 +159,18 @@ resource "aws_vpc_security_group_egress_rule" "allow_ssh_out" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_in" {
+  security_group_id = aws_security_group.admin_ssh_preprod.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_in" {
+  security_group_id = aws_security_group.admin_ssh_preprod.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
