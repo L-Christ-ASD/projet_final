46fa43fce8a99f02d856cc0a740d9501b44e7469
L-Christ-ASD <christ.devops75@gmail.com>
2025-04-08 19:43:12 +0200

    lastest

--- production_tf/main.tf
+++ production_tf/main.tf
@@ -80,23 +80,6 @@ resource "aws_instance" "worker1" {
 }
 
 
-#resource "aws_network_interface" "VIP" {
-#  subnet_id   = "subnet-07ef8d731542349d5"   # ID du sous-réseau
-#  private_ips = ["10.0.1.100"]       # Adresse IP privée
-#  security_groups = [aws_security_group.admin_ssh_production.id]  # ID du groupe de sécurité
-#  depends_on = [aws_security_group.admin_ssh_production, aws_key_pair.vockeyprod]
-#
-#}
-
-#création de la clé SSH pour la prod
-
-# Ajout d'un random_string pour générer un nom unique :
-#resource "random_string" "suffix" {
-#  length  = 4
-#  special = false
-#  upper   = false
-#}
-
 # tls_private_key → Génère une clé privée.
 resource "tls_private_key" "vockeyprod" {
   algorithm = "RSA"
@@ -140,22 +123,7 @@ output "ssh_private_key_filename" {
 }
 
 
-# exportation d'IP masters
-
-#resource "null_resource" "generate_ansible_inventory-masters" {
-#  depends_on = [aws_instance.masters]
-#
-#  provisioner "local-exec" {
-#    command = <<EOT
-#      mkdir -p ../ansible_production
-#      echo "[masters]" >> ../ansible_production/inventory
-#      # Ajouter master1 avec l'IP spécifique
-#      echo "master1 ansible_host=${aws_instance.masters[0].public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./vockeyprod.pem ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" >> ../ansible_production/inventory
-#      # Ajouter les autres masters
-#      ${join("\n", formatlist("echo ansible_host=%s ansible_user=ubuntu ansible_ssh_private_key_file=./vockeyprod.pem ansible_ssh_extra_args='\"-o StrictHostKeyChecking=no\"' >> ../ansible_production/inventory", slice(aws_instance.masters[*].public_ip, 1, length(aws_instance.masters))))}
-#    EOT
-#  }
-#}
+# exportation d'IP masters vers ansible/inventory
 
 resource "null_resource" "generate_ansible_inventory-masters" {
   depends_on = [aws_instance.masters]
@@ -216,6 +184,39 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in_myip" {
   to_port           = 22
 }
 
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in" {
+  for_each          = toset(var.admin-ips)
+  security_group_id = aws_security_group.admin_ssh_production.id
+  cidr_ipv4         = "${each.value}/24"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_ssh_out" {
+  security_group_id = aws_security_group.admin_ssh_production.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_in" {
+  security_group_id = aws_security_group.admin_ssh_production.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_in" {
+  security_group_id = aws_security_group.admin_ssh_production.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
 #========================K8S PORTS================================================
 # temporairement pour le worker k8s
 resource "aws_vpc_security_group_ingress_rule" "allow_port_9345" {
@@ -250,8 +251,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_port_10250" {
   to_port           = 10250
 }
 
-
-
 #=======================================================================
 # permettre le trafic interne entre les ressources qui appartiennent au groupe admin_ssh_production
 resource "aws_security_group_rule" "allow_tcp_2379" {
@@ -272,35 +271,13 @@ resource "aws_security_group_rule" "allow_tcp_2380" {
   source_security_group_id = aws_security_group.admin_ssh_production.id # Groupe de sécurité source
 }
 
-
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_in" {
-  for_each          = toset(var.admin-ips)
+#=============================================================================================
+# ===============Pour la gestion du cluster via ma machine locale (bastion)================
+resource "aws_vpc_security_group_ingress_rule" "allow_6443" {
   security_group_id = aws_security_group.admin_ssh_production.id
-  cidr_ipv4         = "${each.value}/24"
-  from_port         = 22
+  cidr_ipv4         = "${var.mon_ip}/24"
+  from_port         = 6443
   ip_protocol       = "tcp"
-  to_port           = 22
+  to_port           = 6443
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_ssh_out" {
-  security_group_id = aws_security_group.admin_ssh_production.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_http_in" {
-  security_group_id = aws_security_group.admin_ssh_production.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_https_in" {
-  security_group_id = aws_security_group.admin_ssh_production.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
